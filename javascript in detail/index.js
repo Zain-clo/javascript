c601083d5d336c6dc167684e5aa1e7f667ff12ec
Zain-clo <zainablawal2013ab@gmail.com>
2025-01-11 18:09:03 +0100

    error handling and arrow functions

---
+++ javascript in detail/index.js
@@ -0,0 +1,14 @@
+//arrow function
+//fuction add(x,y){
+  //  return x+y
+//}
+let add = (x,y) =>x+y 
+let greet = () => console.log("Hello !!! Happy new year!")
+let sub = (a,b) => a-b
+console.log(add(4,5))
+try{
+    zainab()
+}
+catch (error){
+   console.log(error.message)
+}
